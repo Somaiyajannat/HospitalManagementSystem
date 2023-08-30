bb890aac44b845c5f92b01b05e5e840d624f1dca
Somaiya Jannat <somaiyajannat044@gmail.com>
2023-08-30 14:49:30 +0600

    Data folder have deleted

--- Hospital.Web/Program.cs
+++ Hospital.Web/Program.cs
@@ -1,7 +1,7 @@
 using Hospital.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using Hospital.Web.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<HospitalWebContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
     
 var app = builder.Build();
 
